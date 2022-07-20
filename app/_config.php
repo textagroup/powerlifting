08116e761d088e53ed262ccc32e090b8c2ee6ff6
Kirk Mayo <kirk.mayo@stats.govt.nz>
2022-07-20 16:05:03 +1200

    CSS files were being blocked need a base url

--- app/_config.php
+++ app/_config.php
@@ -1,5 +1,6 @@
 <?php
 
+use SilverStripe\Control\Director;
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
 
@@ -7,3 +8,6 @@ use SilverStripe\Security\Member;
 $validator = PasswordValidator::create();
 // Settings are registered via Injector configuration - see passwords.yml in framework
 Member::set_password_validator($validator);
+if (defined('SS_BASE_URL')) { // set base url for Heroku so css files are not blocked
+    Director::config()->set('alternate_base_url', SS_BASE_URL);
+}
