2e57d1251ac511bd6ddd75676753363265f37f75
Kirk Mayo <kirk.mayo@stats.govt.nz>
2022-08-01 14:58:12 +1200

    Setup LifterClass import export fields

--- app/src/Admin/LiferClassAdmin.php
+++ app/src/Admin/LiferClassAdmin.php
@@ -14,4 +14,22 @@ class LifterClassAdmin extends ModelAdmin
     private static $url_segment = 'lifter-class';
 
     private static $menu_title = 'Lifter Class';
+
+    public function getExportFields()
+    {
+        return [
+            'Title' => 'Title',
+            'MinWeight' => 'MinWeight',
+            'MaxWeight' => 'MaxWeight',
+            'MinAge' => 'MinAge',
+            'MaxAge' => 'MaxAge',
+            'Gender' => 'Gender',
+            'StandardSquat' => 'StandardSquat',
+            'StandardBench' => 'StandardBench',
+            'StandardDeadlift' => 'StandardDeadlift',
+            'StandardTotal' => 'StandardTotal',
+            'Active' => 'Active',
+            'Override' => 'Override',
+        ];
+    }
 }
