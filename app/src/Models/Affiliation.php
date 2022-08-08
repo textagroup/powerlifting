3e09aa1214bae0c0f602ff4932b5d221b47f4f17
Kirk Mayo <kirk.mayo@stats.govt.nz>
2022-08-08 13:19:37 +1200

    Correct table_name for Affiliation

--- app/src/Models/Affiliation.php
+++ app/src/Models/Affiliation.php
@@ -6,7 +6,7 @@ use SilverStripe\ORM\DataObject;
 
 class Affiliation extends DataObject 
 {
-    private static $table_name = 'Afiliation';
+    private static $table_name = 'Affiliation';
 
     private static $db = [
         'Title' => 'Varchar',
